3b21e786043218ecf5c8fcf26e0a075687fd1ba7
Dung <21119301@student.hcmute.edu.vn>
2022-11-20 17:33:17 +0700

    render courses

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,14 @@
+const Course = require('../models/Course')
+const { mongooseToObject } = require('../../until/mongoose')
+class CourseController {
+    //[GET] /courses/:slug
+    show(req, res, next) {
+        Course.findOne({ slug: req.params.slug })
+            .then(courses => {
+                res.render('courses/show', { course: mongooseToObject(courses) })
+            })
+            .catch(next)
+
+    }
+}
+module.exports = new CourseController();
