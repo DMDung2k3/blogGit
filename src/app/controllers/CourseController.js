f13aa4e37600a3c33ef4a0820bfac773e40390dc
Dung <21119301@student.hcmute.edu.vn>
2022-11-21 18:29:13 +0700

    update file Course.js

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -4,11 +4,10 @@ class CourseController {
     //[GET] /courses/:slug
     show(req, res, next) {
         Course.findOne({ slug: req.params.slug })
-            .then(courses => {
-                res.render('courses/show', { course: mongooseToObject(courses) })
+            .then(course => {
+                res.render('courses/show', { course: mongooseToObject(course) })
             })
             .catch(next)
-
     }
 }
 module.exports = new CourseController();
